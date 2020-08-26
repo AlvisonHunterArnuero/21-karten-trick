192eaf292d019e13dcd6e9403cd1e19ee427cee5
Alvison Hunter Arnuero <alvison@gmail.com>
2020-08-26 07:00:32 -0600

    added distro functionality for decks, added state management for shuffles and others

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import CardRows from "./components/CardRows";
 import "./App.css";
 
 const getDeck = () => {
@@ -30,75 +31,129 @@ const getDeck = () => {
   return deck;
 };
 
-const CardButton = () => {
-  return (
-    <div className="col-2 align-self-center">
-      <div className="card border-0">
-        <button
-          type="button"
-          className="btn btn-outline-info w-50 btn-lg p-4 text-uppercase"
-        >
-          Is your Card Here?
-        </button>
-      </div>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onShuffle = this.onShuffle.bind(this);
+    this.onUpdateDeck = this.onUpdateDeck.bind(this);
+    this.state = {
+      chosenRow: "top",
+      numbShuffles: 0,
+      finalArrangement: [],
+      arrCurrentDeck: getDeck(),
+    };
+  }
 
-const Cards = ({ cardName }) => {
-  return (
-    <div className="col-1 my-2 fade-in">
-      <div className="card border-0">
-        <img
-          src={cardName}
-          className="card-img-top text-white"
-          alt={cardName}
-        />
-      </div>
-    </div>
-  );
-};
-const CardRows = ({ deck }) => {
-  return (
-    <div className="row justify-content-between">
-      {deck.map((value, index) => {
-        return <Cards key={index} cardName={value} />;
-      })}
-      <CardButton />
-    </div>
-  );
-};
+  onShuffle = (argChosenRow) => {
+    console.log("User has chosen ", argChosenRow);
+    this.setState((prevState) => {
+      return {
+        numbShuffles: prevState.numbShuffles + 1,
+        chosenRow: argChosenRow,
+      };
+    });
+  };
 
-function App() {
-  let shuffles = 0;
-  let currDeck = getDeck();
-  return (
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        <h1 className="display-4 mt-3 text-info">
-          Welcome to the 21 Karten Trick Game
-        </h1>
+  onUpdateDeck = (argFinalArrangement) => {
+    console.log("Current Arrangement ", argFinalArrangement);
+    this.setState(() => {
+      return {
+        finalArrangement: argFinalArrangement,
+      };
+    });
+  };
+
+  render() {
+    let arrFinal = [];
+    let topDeck = [];
+    let middleDeck = [];
+    let bottomDeck = [];
+    if (this.state.numbShuffles === 0) {
+      topDeck = this.state.arrCurrentDeck.slice(0, 7);
+      middleDeck = this.state.arrCurrentDeck.slice(7, 14);
+      bottomDeck = this.state.arrCurrentDeck.slice(14, 21);
+      arrFinal.push(topDeck, middleDeck, bottomDeck);
+      console.log("First Arrange: ", arrFinal);
+    } else if (this.state.numbShuffles > 0 && this.state.chosenRow === "top") {
+      topDeck = this.state.arrCurrentDeck.slice(7, 14);
+      middleDeck = this.state.arrCurrentDeck.slice(0, 7);
+      bottomDeck = this.state.arrCurrentDeck.slice(14, 21);
+      arrFinal.push(topDeck, middleDeck, bottomDeck);
+      console.log("TOP: ", arrFinal);
+    } else if (
+      this.state.numbShuffles > 0 &&
+      this.state.chosenRow === "middle"
+    ) {
+      topDeck = this.state.arrCurrentDeck.slice(14, 21);
+      middleDeck = this.state.arrCurrentDeck.slice(7, 14);
+      bottomDeck = this.state.arrCurrentDeck.slice(0, 7);
+      arrFinal.push(topDeck, middleDeck, bottomDeck);
+      console.log("MIDDLE: ", arrFinal);
+    } else if (
+      this.state.numbShuffles > 0 &&
+      this.state.chosenRow === "bottom"
+    ) {
+      topDeck = this.state.arrCurrentDeck.slice(0, 7);
+      middleDeck = this.state.arrCurrentDeck.slice(14, 21);
+      bottomDeck = this.state.arrCurrentDeck.slice(7, 14);
+      arrFinal.push(topDeck, middleDeck, bottomDeck);
+      console.log("BOTTOM: ", arrFinal);
+    } else {
+      console.log("FROM ELSE: ", arrFinal);
+    }
+
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <h1 className="display-4 mt-3 text-info">
+            Welcome to the 21 Karten Trick Game
+          </h1>
+        </div>
+        <div className="row justify-content-center">
+          <h4 className="text-warning">
+            Please choose a card and remember it! We will try to guess it!
+          </h4>
+          <strong>{this.state.numbShuffles}</strong>
+        </div>
+
+        {this.state.numbShuffles < 1 ? (
+          <>
+            <CardRows
+              deck={topDeck}
+              onRowSelected={this.onShuffle}
+              selectedDeck={"top"}
+            />
+            <CardRows
+              deck={middleDeck}
+              onRowSelected={this.onShuffle}
+              selectedDeck={"middle"}
+            />
+            <CardRows
+              deck={bottomDeck}
+              onRowSelected={this.onShuffle}
+              selectedDeck={"bottom"}
+            />
+          </>
+        ) : (
+          <>
+            <div className="row justify-content-center">
+              <div className="col-auto fade-in">
+                <div className="card border-0">
+                  {this.state.finalArrangement && (
+                    <img
+                      src={this.state.finalArrangement[10]}
+                      className="card-img-top w-50"
+                      alt={this.state.finalArrangement[10]}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      <div className="row justify-content-center">
-        <h4 className="text-warning">
-          Please choose a card and remember it! We will try to guess it!
-        </h4>
-      </div>
-      {shuffles === 0 ? (
-        <>
-          <CardRows deck={currDeck.slice(0, 7)} />
-          <CardRows deck={currDeck.slice(7, 14)} />
-          <CardRows deck={currDeck.slice(14, 21)} />
-        </>
-      ) : (
-        <>
-          <CardRows deck={currDeck.slice(14, 21)} />
-          <CardRows deck={currDeck.slice(7, 14)} />
-          <CardRows deck={currDeck.slice(0, 7)} />
-        </>
-      )}
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
